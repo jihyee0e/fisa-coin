bb7d13e4cf5fd02293aaeff1c1226edf50e59c7e
jihyee0e <jihyee0e@gmail.com>
2025-02-06 17:30:35 +0900

    코인 정보 업데이트 commit4

--- btc.py
+++ btc.py
@@ -69,7 +69,9 @@ def update_readme(price_info):
 이 리포지토리는 ccdata.io API를 사용하여 비트코인(BTC)의 가격 정보를 자동으로 업데이트합니다.
 ## 현재 비트코인 가격
 > {crypto_info}
+
 ⏳ 업데이트 시간: {now} (UTC)
+
 ---
 자동 업데이트 봇에 의해 관리됩니다.
 """
